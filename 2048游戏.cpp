7806e7a0613fe7d61629da414a11936310f1f8f7
Shen Tianqi <ShenTianqi@users.noreply.github.com>
2017-11-23 00:02:31 +0800

    2048
    重注释

--- 2048游戏.cpp
+++ 2048游戏.cpp
@@ -3,20 +3,20 @@
 #include<ctime>
 #include<cstdio>
 #include<iostream>
-
+//注意：本cpp编译时关闭了vs的sdl安全检查！
 using namespace std;
 int temps = 0;
 int sum = 0;
-int random()
+int random()    //用于产生0、2、4随机数。
 {
-	srand((int)time(NULL));
+	srand((int)time(NULL));//以时间为种子
 	return rand() % 3 * 2;
 }
 
-int Print(int a[][4], int line)
+int Print(int a[][4], int line)//打印二维数组、总分
 {
-	sum = 0;
-	system("cls");
+	sum = 0;              //该全局变量仅用于该函数，我当时在干嘛？
+	system("cls");        //清屏
 	for (int i = 0; i < line; i++)
 	{
 		for (int j = 0; j < 4; j++)
@@ -27,15 +27,15 @@ int Print(int a[][4], int line)
 		cout << endl << endl;
 	}
 	printf("您的总分为%d.\n", sum);
-	return 0;
+	return 0;    //实际上该函数并不需要返回值，可以被定义为void类型。
 }
 
-int Up(int a[4][4])
+int Up(int a[4][4])//同样，事实上不需要返回值。
 {
-	for (int j = 0; j <= 3; j++)
+	for (int j = 0; j <= 3; j++)//移位器
 	{
-		for (int temp = 0; temp <= 6; temp++)
-		{
+		for (int temp = 0; temp <= 6; temp++)//检查6次在于最多可能会有3次相加与3次移位。
+		{                                    //然而问题是实际上并不需要如此之多的检查步骤，完全不需要。
 			for (int i = 3; i >= 1; i--)
 			{
 				if (a[i - 1][j] == 0)
@@ -48,28 +48,28 @@ int Up(int a[4][4])
 					a[i - 1][j] = a[i][j] * 2;
 					a[i][j] = 0;
 				}
-
+				                            //迷之空行，原因可能在于原有else；
 			}
 		}
 	}
 	temps = 0;
-	for (int j = 0; j <= 2; j++)
+	for (int j = 0; j <= 2; j++)//随机数发生器（bug在这里，有时他不一定一定产生一个随机数。）
 	{
-		if (a[3][j] == 0)
+		if (a[3][j] == 0)//该判断用于防止随机数代替原有数字
 			a[3][j] = random();
 		else
 			continue;
 		if (a[3][j] != 0)
 		{
-			++temps; break;
+			++temps; break;//一旦产生随机数，立刻退出发生器，事实上这里使用return语句效率高。
 		}
 	}
-	if (temps == 0 && a[3][3] == 0)
-		a[3][3] = 4;
-	return 0;
+	if (temps == 0 && a[3][3] == 0)//temps判断前面有没有产生过随机数了
+		a[3][3] = 4;               //bug可能就在这里，如果前面产生的均为0且最后一位非0，他不会产生随机数。
+	return 0;                      //a[3][3]=4，鉴于这次随机过程无法保证为非0，只好使用常量了。（实际是懒）
 }
 
-int Left(int a[4][4])
+int Left(int a[4][4])//与up类似，这里存在一个问题，这4个操作函数是可以写到一起的。
 {
 	for (int i = 0; i <= 3; i++)
 	{
@@ -108,7 +108,7 @@ int Left(int a[4][4])
 	return 0;
 }
 
-int Right(int a[4][4])
+int Right(int a[4][4])//同上
 {
 	for (int i = 0; i <= 3; i++)
 	{
@@ -147,7 +147,7 @@ int Right(int a[4][4])
 	return 0;
 }
 
-int Down(int a[4][4])
+int Down(int a[4][4])//同上
 {
 	for (int j = 0; j <= 3; j++)
 	{
@@ -186,13 +186,13 @@ int Down(int a[4][4])
 	return 0;
 }
 
-int PanDing(int a[4][4])
+int PanDing(int a[4][4])//判断这局是否结束。0代表未结束，-1表示结束。
 {
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
 		{
-			if (a[i][j] == 0)
+			if (a[i][j] == 0)//结束条件1：全部非0.
 				return 0;
 		}
 	}
@@ -200,7 +200,7 @@ int PanDing(int a[4][4])
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if (a[i][j] == a[i][j + 1])
+			if (a[i][j] == a[i][j + 1])//结束条件2：全部相邻元素无相同。判断两次，横纵各一次。
 				return 0;
 		}
 	}
@@ -218,16 +218,16 @@ int a[4][4] = { 0 };
 int main()
 {
 	int a1, a2, a3, a4;
-	srand((int)time(NULL));
+	srand((int)time(NULL));//主函数中的随机器，由于仅使用一次，未将其模块化。
 	a1 = rand() % 4; a2 = rand() % 4; a3 = rand() % 4; a4 = rand() % 4;
-	a[a1][a2] = 2; a[a3][a4] = 2;
-	Print(a, 4);
+	a[a1][a2] = 2; a[a3][a4] = 2;//这里存在a1==a3,a2==a4的可能，但不大，我还没跑出来过，假如要重构，这里也要改。
+	Print(a, 4);//第一次输出。
 
-	char ch = 0;
-	while (ch != EOF)
+	char ch = 0;//用于捕获键盘输入。
+	while (ch != EOF)//事实上这是一个不可能的结束条件，键盘一个键输入不了EOF（-1）.
 	{
 		ch = getch();
-		if (ch == 72)
+		if (ch == 72)//实际上这里可以使用switch语句。
 			Up(a);
 		else if (ch == 75)
 			Left(a);
@@ -236,15 +236,15 @@ int main()
 		else if (ch == 80)
 			Down(a);
 		else
-			continue;
+			continue;//跳过所有错误输入。
 		Print(a, 4);
 
-		if (PanDing(a) == -1)
+		if (PanDing(a) == -1)//捕获到结束条件时操作。
 		{
 			printf("您已无路可走。");
 			break;
 		}
 	}
-	system("pause");
+	system("pause");//防止一闪而过的结束语句与一脸懵逼的用户。
 	return 0;
 }
